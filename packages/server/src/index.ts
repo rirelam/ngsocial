0378e14ebe2dcfad39a17437ae6e7a991e773551
Ricardo Lamar Meneses <ricardo.lamar@gmail.com>
2022-09-13 13:10:01 -0400

    implementa mock data usando casual

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,12 +1,67 @@
-import express, { Application } from "express";
-import { ApolloServer } from "apollo-server-express";
+import express, { Application } from "express";import { ApolloServer } from "apollo-server-express";import casual from "casual";import schema from "./graphql/schema";
+let postsIds: string[] = [];
+let usersIds: string[] = [];
 
-import schema from "./graphql/schema";
+const mocks = {
+  User: () => ({
+    id: () => {
+      let uuid = casual.uuid;
+      usersIds.push(uuid);
+      return uuid;
+    },
+    fullName: casual.full_name,
+    bio: casual.text,
+    email: casual.email,
+    username: casual.username,
+    password: casual.password,
+    image: "https://picsum.photos/seed/picsum/200/300",
+    coverImage: "https://picsum.photos/seed/picsum/200/300",
+    postsCount: () => casual.integer(0),
+  }),
+  Post: () => ({
+    id: () => {
+      let uuid = casual.uuid;
+      postsIds.push(uuid);
+      return uuid;
+    },
+    author: casual.random_element(usersIds),
+    text: casual.text,
+    image: "https://picsum.photos/seed/picsum/200/300",
+    commentsCount: () => casual.integer(0),
+    likesCount: () => casual.integer(0),
+    latestLike: casual.first_name,
+    createdAt: () => casual.date(),
+  }),
+  Comment: () => ({
+    id: casual.uuid,
+    author: casual.random_element(usersIds),
+    comment: casual.text,
+    post: casual.random_element(postsIds),
+    createdAt: () => casual.date(),
+  }),
+  Like: () => ({
+    id: casual.uuid,
+    post: casual.random_element(postsIds),
+    user: casual.random_element(usersIds),
+  }),
+  Query: () => ({
+    getPostsByUserId: () => [...new Array(casual.integer(10, 100))],
+    getFeed: () => [...new Array(casual.integer(10, 100))],
+    getNotificationsByUserId: () => [...new Array(casual.integer(10, 100))],
+    getCommentsByPostId: () => [...new Array(casual.integer(10, 100))],
+    getLikesByPostId: () => [...new Array(casual.integer(10, 100))],
+    searchUsers: () => [...new Array(casual.integer(10, 100))],
+  }),
+};
 
 async function startApolloServer() {
   const PORT = 8080;
   const app: Application = express();
-  const server: ApolloServer = new ApolloServer({ schema });
+  const server: ApolloServer = new ApolloServer({
+    schema,
+    mocks: true,
+    mockEntireSchema: false
+  });
 
   await server.start();
 
