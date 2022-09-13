d1b12f62919a081fdf8a859e4001dc2438f1b08d
Ricardo Lamar Meneses <ricardo.lamar@gmail.com>
2022-09-13 10:39:05 -0400

    configurado simple servidor del graphql

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,12 +1,37 @@
-import express, { Application } from 'express';
+import express, { Application } from "express";import { ApolloServer, Config, gql } from "apollo-server-express";
+import { IResolvers } from "@graphql-tools/utils";
 
-const PORT = 8080;
-const app: Application = express();
+const typeDefs = gql`
+  type Query {
+    message: String!
+  }
+`;
 
-app.get("/", (req, res) => res.send(`Express is successfully running!`));
+const resolvers: IResolvers = {
+  Query: {
+    message: () => "It works!",
+  },
+};
 
-//
+const config: Config = {
+  typeDefs: typeDefs,
+  resolvers: resolvers,
+};
 
-app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
-});
+async function startApolloServer(config: Config) {
+  const PORT = 8080;
+  const app: Application = express();
+
+  const server: ApolloServer = new ApolloServer(config);
+  await server.start();
+  server.applyMiddleware({
+    app,
+    path: "/graphql",
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+  });
+}
+
+startApolloServer(config);
