28fbe2756d57a8ccef1413e553b409c12c275a2f
Ricardo Lamar Meneses <ricardo.lamar@gmail.com>
2022-09-13 10:09:29 -0400

    completando scripts para arranque en dev y compilacion del proyecto

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -5,6 +5,8 @@ const app: Application = express();
 
 app.get("/", (req, res) => res.send(`Express is successfully running!`));
 
+//
+
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
